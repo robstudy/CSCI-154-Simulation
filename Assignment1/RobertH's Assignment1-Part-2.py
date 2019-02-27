ff9b91669754ec3f0453b878ed7b5684aa720099
roberthov <48002190+roberthov@users.noreply.github.com>
2019-02-26 22:21:25 -0800

    Made it work with contraction words, and greatly simplified code.

--- Assignment1/RobertH's Assignment1-Part-2.py
+++ Assignment1/RobertH's Assignment1-Part-2.py
@@ -14,36 +14,25 @@ class Scrambler:
 				break
 			else:
 				newWords=newLine.split()#Otherwise split up each line into a list of words,
-				newWords.reverse()#in reverse order.
-				correctedWords=list()#This will contain the words we will push into the final result.
-				i=0
-				while(i<len(newWords)):#Iterating over all the words in the list.
-					if newWords[i][-1]=="!" or newWords[i][-1]=="," or newWords[i][-1]=="." or newWords[i][-1]=="?":#If the current word ends in a punctuating character
-						correctedWords.append(newWords[i][-1])#we add it to the tail of the list, which is really the head in reverse order
-						newWords[i]=newWords[i][:-1]#we slice off the punctuating character from the word
-						i-=1#we decrement i by one because we want to redo this iteration of the loop, with the same word shortened.
-					else:#If there were no special characters at the end
-						correctedWords.append(newWords[i])#Just add it to the tail of the list.
-					i+=1#Increment the loop.
-
-				correctedWords.reverse()#Reverse the list of corrected words
-				self.words.extend(correctedWords)#and append all of its elements to the end of the class's words.
+				self.words.extend(newWords)#and append all of its elements to the end of the class's words.
 		#print(self.words)#Prints the list, for debugging purposes.
 
 	def scramble_it(self):
 		returnString=""#This will contain the string that will be returned.
 		for w in self.words:#For all the words that we have
-			if w!="!" and w!="," and w!="." and w!="?":#If the word isn't a single punctuating character
-				returnString+=" "#Add a space before it. We will remove the extra leading space caused by this at the end.
-			newWord=list(w[1:-1])#Grab the middle of the current word. If the word is two character or less, this and the next line will just shuffle an empty list.
+			returnString+=" "
+			endIndex=-1#The index of the character we consider to be the index after the middle of the word.
+			for i in range(len(w)-1,0,-1):#For loop, starting from the end of the word going until the beginning.
+				if w[i]=="!" or w[i]=="," or w[i]=="." or w[i]=="?" or w[i]=="'" or w[i]=='"':#If we find one of these characters
+					endIndex=i-1#update the end index. So "That's" should consider the second 't' the start of the end, the t's won't be shuffled.
+			newWord=list(w[1:endIndex])#Grab the middle of the current word. If the word is two character or less, this and the next line will just shuffle an empty list.
 			shuffle(newWord)#Shuffle just the middle
 			newWord.insert(0,w[0])#Add the first character of the word back to it at the front.
 			if len(w)>1:#If the word is more than one character, that is to say if it has a last character that is different from the first.
-				newWord.append(w[-1])#add the last character back to it at the end.
+				newWord.append(w[endIndex:])#add the last character back to it at the end.
 			returnString+=''.join(newWord)#Convert the newWord from a list to a string, and concatenate it with the returnString.
-		if(len(returnString)>1 and returnString[0]==" "):#If there is at least one word and it contains a leading space
-			returnString=returnString[1:]#slice off the leading space.
-		return returnString#return the space.
+		returnString=returnString[1:]#Slice off the leading space.
+		return returnString#return the string.
 
 
 
